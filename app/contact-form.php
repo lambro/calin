c6c592005f9787cd2dbf1a2e686315b29e0d0ca5
Laura Ambrose <lauraambrose@gmail.com>
2017-07-09 11:50:01 +0800

    finished contact form and removed unused images

---
+++ app/contact-form.php
@@ -0,0 +1,40 @@
+<?php
+require_once '../PHPMailer-master/PHPMailerAutoload.php';
+
+if (isset($_POST['name']) && isset($_POST['companyName']) && isset($_POST['emailAddress']) && isset($_POST['customerPhoneNumber']) && isset($_POST['enquiryText'])) {
+
+    //check if any of the inputs are empty
+    if (empty($_POST['name']) || empty($_POST['companyName']) || empty($_POST['emailAddress']) || empty($_POST['customerPhoneNumber']) || empty($_POST['enquiryText'])) {
+        $data = array('success' => false, 'message' => 'Please fill out the form completely.');
+        echo json_encode($data);
+        exit;
+    }
+
+    //create an instance of PHPMailer
+    $mail = new PHPMailer();
+
+    $mail->From = $_POST['emailAddress'];
+    $mail->FromName = $_POST['name'];
+    $mail->AddAddress('receive_test@laura-the.ninja'); //recipient 
+    $mail->Subject = "Message from the Website";
+    $mail->Body = "Name: " . $_POST['name'] ."\r\n\r\nCompany: " . $_POST['companyName'] . "\r\n\r\nMessage: " . stripslashes($_POST['enquiryText']);
+
+    if (isset($_POST['ref'])) {
+        $mail->Body .= "\r\n\r\nRef: " . $_POST['ref'];
+    }
+
+    if(!$mail->send()) {
+        $data = array('success' => false, 'message' => 'Message could not be sent. Mailer Error: ' . $mail->ErrorInfo);
+        echo json_encode($data);
+        exit;
+    }
+
+    $data = array('success' => true, 'message' => 'Thanks! We have received your message.');
+    echo json_encode($data);
+
+} else {
+
+    $data = array('success' => false, 'message' => 'Please fill out the form completely.');
+    echo json_encode($data);
+
+}
--- app/scripts/controllers/contact-us.js
+++ app/scripts/controllers/contact-us.js
@@ -8,20 +8,47 @@
  * Controller of the calinApp
  */
 angular.module('calinApp')
-  .controller('ContactUsCtrl', function ($scope) {
+  .controller('ContactUsCtrl', function ($scope, $http) {
 
     $scope.user = {};
+    $scope.result = 'hidden'
+    $scope.resultMessage;
+    $scope.formData; //formData is an object holding the name, email, subject, and message
+    $scope.submitButtonDisabled = false;
+    $scope.submitted = false; //used so that form errors are shown only after the form has been submitted
 
-    $scope.submitForm = function(isValid) {
+    $scope.submitForm = function(contactform) {
+      console.log(arguments);
+        $scope.submitted = true;
+        $scope.submitButtonDisabled = true;
+        if (contactform.$valid) {
+            console.log("form is valid");
+            console.log($scope.userForm);
+            $http({
+                method  : 'POST',
+                url     : 'contact-form.php',
+                data    : angular.element.param($scope.user),  //param method from jQuery
+                headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  //set the headers so angular passing info as form data (not request payload)
+            }).then(function(data){
+                
+                console.log("there was no error");
 
-    // check to make sure the form is completely valid
-    if (isValid) {
-      alert('our form is amazing');
-    }
+                console.log(data);
 
-    };
+                $scope.submitButtonDisabled = true;
+                $scope.resultMessage = data.message;
+                $scope.success == true ;
+            });
+        } else {
+          $scope.submitButtonDisabled = false;
+          $scope.resultMessage = data.message;
+          $scope.result='bg-danger';
 
-
-
-
-  });
+          console.log(formData);
+          console.log("there was an error");
+            $scope.submitButtonDisabled = false;
+            $scope.resultMessage = 'Failed <img src="http://www.chaosm.net/blog/wp-includes/images/smilies/icon_sad.gif" alt=":(" class="wp-smiley">  Please fill out all the fields.';
+            $scope.result='bg-danger';
+          }
+        };
+      });
